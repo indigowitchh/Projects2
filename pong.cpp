121e8bc9b59be4041f8dc5d2302f5b94d598ced7
do i give a darn <114239691+indigowitchh@users.noreply.github.com>
2023-01-09 09:56:08 -0700

    Create pong.cpp

---
+++ pong.cpp
@@ -0,0 +1,62 @@
+// Demonstrate primitve drawing in SFML
+#include<iostream>
+using namespace std;
+
+
+#include "SFML/Graphics.hpp"
+
+int main() {
+
+    //game set up (you'll need these two lines in every game)
+    sf::RenderWindow renderWindow(sf::VideoMode(500, 500), "Pong Paddle"); //set up screen
+    sf::Event event; //set up event queue
+
+    //paddle1 set up
+    sf::RectangleShape paddle1(sf::Vector2f(20.0f, 100.0f));
+    paddle1.setFillColor(sf::Color::Blue); //other colors: https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Color.php
+    paddle1.setPosition(30.0f, 250.0f); //set position: this is where the top left corner will be
+    //paddle2 set up
+    sf::RectangleShape paddle2(sf::Vector2f(20.0f, 100.0f));
+    paddle2.setFillColor(sf::Color::Blue); //other colors: https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Color.php
+    paddle2.setPosition(450.0f, 250.0f); //set position: this is where the top left corner will be
+    //ball set up
+    sf::CircleShape ball(15); //sets radius of circle
+    ball.setFillColor(sf::Color(200, 50, 50)); //numbers and color names
+    ball.setPosition(225, 300); //position
+
+
+    //################### HOLD ONTO YOUR BUTTS, ITS THE GAME LOOP###############################################################
+    while (renderWindow.isOpen()) {//keep window open until user shuts it down
+        while (renderWindow.pollEvent(event)) { //look for events
+
+            //this checks if the user has clicked the little "x" button in the top right corner
+            if (event.type == sf::Event::EventType::Closed)
+                renderWindow.close();
+
+            //KEYBOARD INPUT (just one key to start
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { //checks if "W" is pressed
+                paddle1.move(0, -5);
+            } //move the rectangle 5 pixels UP on the y axis
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { //checks if "s" is pressed
+                paddle1.move(0, 5); //move the rectangle 5 pixels UP on the y axis
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { //checks if "up" is pressed
+                paddle2.move(0, -5);
+            } //move the rectangle 5 pixels UP on the y axis
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { //checks if "down" is pressed
+                paddle2.move(0, 5); //move the rectangle 5 pixels UP on the y axis
+            }
+
+            //render section-----------------------------------------
+            renderWindow.clear(); //wipes screen, without this things smear
+            renderWindow.draw(paddle1); //you gotta drew each object
+            renderWindow.draw(paddle2); //you gotta drew each object
+            renderWindow.draw(ball);
+            renderWindow.display(); //flips memory drawings onto screen
+
+        }//######################## end game loop ###################################################################################
+
+        
+    }
+    cout << "goodbye!" << endl;
+} //end game
